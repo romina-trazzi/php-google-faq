3850d9839af1d83618a15d800f46000b6d0886c9
Romina Trazzi <romina.trazzi@posta.it>
2021-02-03 16:55:28 +0100

    Struttura iniziale

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <!-- meta properties -->
+        <meta charset="utf-8">
+        <meta name="author" content="Romina Trazzi">
+        <meta name="description" content="Esercizio Google Faqs con Sass e PHP">
+        <meta name="keywords" content="php, Google, faqs, sass, PHP">
+        <!-- / meta properties -->
+        
+        <!--Axios CDN -->
+        <!-- <script src="https://unpkg.com/axios/dist/axios.min.js"></script> -->
+        <!-- / Axios CDN -->
+        
+        <title>Esercizio Google Faqs</title>
+
+        <!-- fogli di stile -->
+        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.2/handlebars.min.js"></script> -->
+        <link rel="stylesheet" href="dist/css/style.css" media="screen, all">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />  
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&display=swap">
+        <!-- / fogli di stile -->
+    </head>
+
+    <body>
+        <nav>
+            <div class="logo">
+                <img src="dist/img/google-logo.jpg" alt="logo di Google">
+                <h3> Privacy e termini </h3>
+            </div>
+            <div class="navbar">
+                <ul>
+                    <li><a href=#>Introduzione</a></li>
+                    <li><a href=#>Norme sulla privacy</a></li>
+                    <li><a href=#>Termini di servizio</a></li>
+                    <li><a href=#>Domande frequenti</a></li>
+                </ul>
+            </div>
+        </nav>
+
+
+    </body>
+
+</html>
+
