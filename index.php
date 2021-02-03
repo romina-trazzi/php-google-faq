5bfe54457218ca0e8f84566792bb896a84770ee4
Romina Trazzi <romina.trazzi@posta.it>
2021-02-04 00:30:49 +0100

    Commit navbar

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
+$title = [
+    "title_1" => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
+    "title_2" => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+    "title_3" => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+    "title_4" => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?"
+]
+
 
 ?>
 
@@ -13,15 +20,10 @@
         <meta name="description" content="Esercizio Google Faqs con Sass e PHP">
         <meta name="keywords" content="php, Google, faqs, sass, PHP">
         <!-- / meta properties -->
-        
-        <!--Axios CDN -->
-        <!-- <script src="https://unpkg.com/axios/dist/axios.min.js"></script> -->
-        <!-- / Axios CDN -->
-        
+                
         <title>Esercizio Google Faqs</title>
 
         <!-- fogli di stile -->
-        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.2/handlebars.min.js"></script> -->
         <link rel="stylesheet" href="dist/css/style.css" media="screen, all">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />  
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&display=swap">
@@ -29,6 +31,8 @@
     </head>
 
     <body>
+
+        <!-- Navbar -->
         <nav>
             <div class="logo">
                 <img src="dist/img/google-logo.jpg" alt="logo di Google">
@@ -39,10 +43,13 @@
                     <li><a href=#>Introduzione</a></li>
                     <li><a href=#>Norme sulla privacy</a></li>
                     <li><a href=#>Termini di servizio</a></li>
-                    <li><a href=#>Domande frequenti</a></li>
+                    <li class="faq"><a href=#>Domande frequenti</a></li>
                 </ul>
             </div>
         </nav>
+        <!-- / Navbar -->
+
+
 
 
     </body>
